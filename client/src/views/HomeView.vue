bbc9d94abdd18e3eeff5a077eaa3dbc16c15ea10
rhyzen5127 <102752650+rhyzen5127@users.noreply.github.com>
2022-12-08 01:50:29 +0700

    Create NavBar.vue.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -7,12 +7,14 @@ import { defineComponent } from 'vue';
 
 // Components
 import HelloWorld from '../components/HelloWorld.vue';
+import NavBar from '../components/NavBar.vue'
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
     HelloWorld,
+    NavBar,
   },
 });
 </script>
