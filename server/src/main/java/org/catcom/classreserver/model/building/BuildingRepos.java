e9c528c28f6be7e8dc357df4c731ba4cef5482f7
WSKKung <armkung493@gmail.com>
2022-12-12 23:00:11 +0700

    Clear backend folder

--- server/src/main/java/org/catcom/classreserver/model/building/BuildingRepos.java
+++
@@ -1,6 +0,0 @@
-package org.catcom.classreserver.model.building;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BuildingRepos extends CrudRepository<Building, Integer> {
-}
