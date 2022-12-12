35e24f8d82193df188e58aec973a067bd7b58781
WSKKung <armkung493@gmail.com>
2022-12-12 20:45:03 +0700

    Added classroom manager

---
+++ server/src/main/java/org/catcom/classreserver/exceptions/ClassroomException.java
@@ -0,0 +1,9 @@
+package org.catcom.classreserver.exceptions;
+
+public class ClassroomException extends Exception
+{
+    public ClassroomException(String message)
+    {
+        super(message);
+    }
+}
