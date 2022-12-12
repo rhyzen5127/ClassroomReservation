709d22553f45084071160ae494caaccdced7507b
WSKKung <armkung493@gmail.com>
2022-12-12 20:44:43 +0700

    Added classroom detail fields

--- server/src/main/java/org/catcom/classreserver/model/classroom/Classroom.java
+++ server/src/main/java/org/catcom/classreserver/model/classroom/Classroom.java
@@ -4,7 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import net.minidev.json.annotate.JsonIgnore;
 import org.catcom.classreserver.model.building.Building;
+import org.catcom.classreserver.model.reservation.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "classrooms")
@@ -18,17 +23,42 @@ public class Classroom
     @Getter @Setter
     private String name;
 
+    @Column
+    @Getter @Setter
+    private String level;
+
     @Column(nullable = false)
     @Getter @Setter
     private String status;
 
+    // width in meter unit (m)
     @Column
     @Getter @Setter
-    private String level;
+    private Integer width;
+
+    // height in meter unit (m)
+    @Column
+    @Getter @Setter
+    private Integer height;
+
+    @Column
+    @Getter @Setter
+    private Integer seats;
 
     @ManyToOne()
     @JoinColumn(name = "building_id", referencedColumnName = "id")
     @Getter @Setter
     private Building building;
+
+    // one-to-many relationship collections
+
+    @OneToMany(targetEntity = Reservation.class, mappedBy = "room", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public boolean isReady()
+    {
+        return "ready".equalsIgnoreCase(status);
+    }
 
 }
