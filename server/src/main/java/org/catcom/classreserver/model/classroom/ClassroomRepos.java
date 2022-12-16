7ff613724e9607d18cb47383d3386830c196f3cc
WSKKung <armkung493@gmail.com>
2022-12-16 12:56:43 +0700

    Changed to JpaRepository, add specs for query

--- server/src/main/java/org/catcom/classreserver/model/classroom/ClassroomRepos.java
+++ server/src/main/java/org/catcom/classreserver/model/classroom/ClassroomRepos.java
@@ -1,6 +1,6 @@
 package org.catcom.classreserver.model.classroom;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassroomRepos extends CrudRepository<Classroom, Integer> {
+public interface ClassroomRepos extends JpaRepository<Classroom, Integer> {
 }
--- server/src/main/java/org/catcom/classreserver/model/reservation/ReservationRepos.java
+++ server/src/main/java/org/catcom/classreserver/model/reservation/ReservationRepos.java
@@ -1,12 +1,20 @@
 package org.catcom.classreserver.model.reservation;
 
+import org.catcom.classreserver.model.classroom.Classroom;
+import org.catcom.classreserver.model.user.User;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ReservationRepos extends CrudRepository<Reservation, Integer>
+public interface ReservationRepos extends JpaRepository<Reservation, Integer>, JpaSpecificationExecutor<Reservation>
 {
     @Query("SELECT r FROM Reservation r WHERE r.room.id = ?1")
     Iterable<Reservation> findByRoomId(int roomId);
+
+    @Query("SELECT r FROM Reservation r WHERE r.room.id = ?1 AND r.status = ?2")
+    Iterable<Reservation> findByRoomIdAndStatus(int ownerId, String status);
 
     @Query("SELECT r FROM Reservation r WHERE r.owner.id = ?1")
     Iterable<Reservation> findByOwnerId(int ownerId);
@@ -16,5 +24,17 @@ public interface ReservationRepos extends CrudRepository<Reservation, Integer>
 
     @Query("SELECT r FROM Reservation r WHERE r.status = ?1")
     Iterable<Reservation> findByStatus(String status);
+
+    static Specification<Reservation> hasOwner(User user) {
+        return (r, cq, cb) -> cb.equal(r.get("owner"), user);
+    }
+
+    static Specification<Reservation> hasStatus(String status) {
+        return (r, cq, cb) -> cb.equal(r.get("status"), status);
+    }
+
+    static Specification<Reservation> forRoom(Classroom classroom) {
+        return (r, cq, cb) -> cb.equal(r.get("room"), classroom);
+    }
 
 }
